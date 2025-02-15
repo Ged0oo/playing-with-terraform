0aa72d3b6a32b179928c1f183c8fedb90a1bd1f7
Mohamed Nagy <mohmmednagy@gmail.com>
2025-02-15 17:27:03 +0200

    add vpc, subnet, internetgateway, routetable

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,54 @@
+// Defines AWS as the infrastructure provider
 provider "aws" {
   region = "us-east-1"
 }
 
+// Defines Virtual Private Cloud (VPC)
+resource "aws_vpc" "myvpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "MyVPC"
+  }
+}
+
+// Creates a subnet 
+resource "aws_subnet" "mysubnet" {
+  vpc_id                  = aws_vpc.myvpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "MySubnet"
+  }
+}
+
+// Attaches an Internet Gateway to the VPC 
+// to allow public internet access.
+resource "aws_internet_gateway" "myigw" {
+  vpc_id = aws_vpc.myvpc.id
+  tags = {
+    Name = "MyInternetGateway"
+  }
+}
+
+// Creates a Route Table within the VPC.
+resource "aws_route_table" "myrt" {
+  vpc_id = aws_vpc.myvpc.id
+  tags = {
+    Name = "MyRouteTable"
+  }
+}
+
+// Define Route for Internet Access
+resource "aws_route" "default_route" {
+  	route_table_id         = aws_route_table.myrt.id
+  	gateway_id             = aws_internet_gateway.myigw.id
+    destination_cidr_block = "0.0.0.0/0"
+}
+
+// Associates the route table with the subnet
+// enabling internet access for instances in this subnet.
+resource "aws_route_table_association" "myrta" {
+  subnet_id      = aws_subnet.mysubnet.id
+  route_table_id = aws_route_table.myrt.id
+}
